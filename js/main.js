b19c173b477dc4e7983f095aff8f971c17b9410a
Chao-Ming Chang <cmc4010@gmail.com>
2016-03-19 16:32:39 +0800

    tried language change with javascript

--- js/main.js
+++ js/main.js
@@ -12,3 +12,29 @@ $( document ).ready(
 		$(".specialHold0").height($("#myNav").height() + 2);
 	}
 );
+
+$( window ).resize(
+	function(){
+		$(".specialHold0").height($("#myNav").height() + 2);		
+	}
+);
+
+function englify(){
+	document.getElementById("pageTitle").innerHTML = "PORTFOLIO";
+	document.getElementById("pageTitle").style.fontSize = "30px";
+	document.getElementById("myName").innerHTML = "<small>Chao-Ming Chang</small>";
+	document.getElementById("recentWorkText").innerHTML = "Recent Work";
+	document.getElementById("workInProgress").innerHTML = "Work in progress ...";
+	$(".specialHold0").height($("#myNav").height() + 2);
+}
+
+function chinafy(){
+	// $("pageTitle").html("作品集");
+	// $("myName").html( "<small>張晁銘</small>" );
+	document.getElementById("pageTitle").innerHTML = "作品集";
+	document.getElementById("pageTitle").style.fontSize = "35px";
+	document.getElementById("myName").innerHTML = "<small>張晁銘</small>";
+	document.getElementById("recentWorkText").innerHTML = "進期作品";
+	document.getElementById("workInProgress").innerHTML = "網站開發中 ...";
+	$(".specialHold0").height($("#myNav").height() + 2);
+}
